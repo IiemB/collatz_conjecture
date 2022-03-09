b7c17b37dfc6ca9cb413f5d04228d1c37c901fc1
Iiem Bayyinullah <bayyinullah@gmail.com>
2022-03-09 09:17:36 +0700

    update app icon, splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Future.delayed(const Duration(seconds: 5));
+
+  FlutterNativeSplash.remove();
+
   runApp(const MyApp());
 }
 
